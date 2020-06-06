af0fdcc09f25e121f88b4ff9c5e7f7f1d7700fe3
Lan Phan <lanc_phan@hotmail.com>
2020-06-06 16:12:15 -0400

    connect the Apollo Client to React components with ApolloProvider

--- src/index.js
+++ src/index.js
@@ -5,25 +5,27 @@ import App from './App';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: 'https://graphql-pokemon.now.sh/'
-})
+  uri: 'https://graphql-pokemon.now.sh/',
+});
 
 const client = new ApolloClient({
   cache,
-  link
-})
+  link,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
