52d24053b0d5e1ff0eb4b537d66c3eb7edf0c8ef
Lan Phan <lanc_phan@hotmail.com>
2020-06-06 16:02:28 -0400

    create an instance for InMemoryCache, HttpLink with the pokemon uri, and send it to ApolloClient

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://graphql-pokemon.now.sh/'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
 
 ReactDOM.render(
   <React.StrictMode>
